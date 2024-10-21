c94c31ccf2d4e4dacde4fedadd47fb990e392a7b
Jake Bowen <jakeeb05@gmail.com>
2024-10-20 21:10:21 -0700

    Initial Commit

---
+++ merge_sort_search/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  merge_sort_search
+//
+//  Created by Jake Bowen on 10/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
